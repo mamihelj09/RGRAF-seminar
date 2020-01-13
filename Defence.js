f87848c991a0cdffd6c47e71d0fa802dfdef0be8
Marko Mihelj <marko.mihelj@infinum.hr>
2020-01-13 19:42:46 +0100

    add smaller planet

--- Defence.js
+++ Defence.js
@@ -4,7 +4,7 @@ export class Defence {
   constructor(model, position) {
     this._model = model;
     this._model.position.copy(position);
-    this._model.scale.set(8, 8, 8);
+    this._model.scale.set(60, 60, 60);
 
     this._boundery = new THREE.BoxHelper(this._model);
     this._boundery.geometry.computeBoundingBox();
